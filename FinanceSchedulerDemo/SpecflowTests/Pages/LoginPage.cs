2279fb78289553dd06fecc9eaa122eaae2a41ef7
a.zhadetsky <a.zhadetsky@itransition.com>
2022-01-20 19:35:19 +0300

    [FIN] Specflow test added.

---
+++ FinanceSchedulerDemo/SpecflowTests/Pages/LoginPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System.Threading;
+
+namespace SpecflowTests.Pages
+{
+    public class LoginPage
+    {
+        public IWebDriver webDriver { get; }
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public IWebElement LoginTab => webDriver.FindElement(By.Id("loginTab"));
+        public IWebElement UserNameInput => webDriver.FindElement(By.Id("userName"));
+        public IWebElement PasswordInput => webDriver.FindElement(By.Id("password"));
+        public IWebElement LoginBtn => webDriver.FindElement(By.Id("loginButton"));
+        public IWebElement UserAccountInfoCard => webDriver.FindElement(By.Id("userInfoCard"));
+
+        public void ClickLoginTab()
+        {
+            LoginTab.Click();
+        }
+
+        public void EnterCredentials(string userName, string password)
+        {
+            UserNameInput.SendKeys(userName);
+            PasswordInput.SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            Thread.Sleep(1000); // change with selenium wait
+            LoginBtn.Click();
+        }
+
+        public bool IsUserLoggedInSuccessfully()
+        {
+            Thread.Sleep(1000);
+            return UserAccountInfoCard.Displayed;
+        }
+    }
+}
---
+++ FinanceSchedulerDemo/SpecflowTests/Steps/LoginSteps.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecflowTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecflowTests.Steps
+{
+    [Binding]
+    public sealed class LoginSteps
+    {
+        LoginPage loginPage = null; // DI it also
+        IWebDriver webDriver;
+
+        public LoginSteps()
+        {
+            var chromeOptions = new ChromeOptions(); // DI it and all other
+            chromeOptions.AddArguments("--headless", "--disable-gpu", "--window-size=1920,1080");
+            webDriver = new ChromeDriver(chromeOptions);
+        }
+
+        [Given(@"I launch the app")]
+        public void GivenILaunchTheApp()
+        {
+            webDriver.Navigate().GoToUrl("http://localhost:5000/");
+            loginPage = new LoginPage(webDriver);
+        }
+
+        [Given(@"I click the Sign In button")]
+        public void GivenIClickTheSignInButton()
+        {
+            loginPage.ClickLoginTab();
+        }
+
+        [Given(@"I enter the following credentials")]
+        public void GivenIEnterTheFollowingCredentials(Table table)
+        {
+            dynamic credentials = table.CreateDynamicInstance();
+
+            loginPage.EnterCredentials((string)credentials.Username, (string)credentials.Password);
+        }
+
+        [Given(@"I click submit button button")]
+        public void GivenIClickSubmitButtonButton()
+        {
+            loginPage.ClickLoginButton();
+        }
+
+        [Then(@"I should see the user account page")]
+        public void ThenIShouldSeeTheUserAccountPage()
+        {
+            Thread.Sleep(500);
+            Assert.That(loginPage.IsUserLoggedInSuccessfully(), Is.True);
+        }
+
+    }
+}
